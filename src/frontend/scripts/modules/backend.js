55a80a0ddde675f1eac62944c460be938ddf47b4
wangyan <525677808@qq.com>
2018-08-24 14:43:07 +0800

    Update backend.js

--- src/frontend/scripts/modules/backend.js
+++ src/frontend/scripts/modules/backend.js
@@ -1,1 +1,18 @@
 
+(function (doc, win) {
+    let docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = () => {
+            let clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if (clientWidth > 575) {
+                docEl.style.fontSize = '16px';
+            } else if (clientWidth <= 575) {
+                docEl.style.fontSize = 20 * (clientWidth / 575) + 'px';
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
